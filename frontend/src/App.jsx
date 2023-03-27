ff68b95f82c80171587e34a97707661757ae4aab
Dang Le <haidang.levn@gmail.com>
2023-03-27 14:45:17 +0300

    Test fetching API data in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,67 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [businessData, setBusinessData] = useState([]);
+  const capitalRegionPostalCode = [
+    "02100",
+    "00140",
+    "00930",
+    "00710",
+    "01730",
+    "00500",
+    "01760",
+    "01690",
+    "00510",
+    "00180",
+  ];
+  const fetchAPIdata = async () => {
+    console.log("Fetching API data");
+
+    for (const code of capitalRegionPostalCode) {
+      await axios
+        .get(
+          `http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=20&resultsFrom=0&streetAddressPostCode=${code}&companyRegistrationFrom=2014-02-28`
+        )
+        .then((res) => {
+          console.log(`Result for postal code: ${code}`);
+          console.log(res.data.results);
+          console.log("---------------------------------");
+        });
+    }
+    /* capitalRegionPostalCode.map((code) => {
+      axios
+        .get(
+          `http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=2&resultsFrom=0&streetAddressPostCode=${code}&companyRegistrationFrom=2014-02-28`
+        )
+        .then((res) => {
+          console.log(`Result for postal code: ${code}`);
+          console.log(res.data.results);
+          console.log("---------------------------------");
+        });
+    }); */
+  };
+  useEffect(() => {
+    axios.get("http://localhost:4000").then((res) => {
+      console.log("backend connected");
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>API Client</h1>
-    </div>
+      <p>Press the below button to fetch companies data from the API:</p>
+
+      <button onClick={fetchAPIdata}>Fetch API data</button>
+      <div>
+        {businessData.map((business) => {
+          return <p>{business.name}</p>;
+        })}
+      </div>
+      <p>Output: </p>
+{/*       {businessData.map()}
+ */}    </div>
   );
 }
 

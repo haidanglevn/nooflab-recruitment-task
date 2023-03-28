a60ca10c661896069979ae51a4e7a1c78ca50983
Dang Le <haidang.levn@gmail.com>
2023-03-28 21:37:24 +0300

    Add test sync, post and get function to the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,46 +22,84 @@ function App() {
     for (const code of capitalRegionPostalCode) {
       await axios
         .get(
-          `http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=20&resultsFrom=0&streetAddressPostCode=${code}&companyRegistrationFrom=2014-02-28`
+          `http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=1&resultsFrom=0&streetAddressPostCode=${code}&companyRegistrationFrom=2014-02-28`
         )
         .then((res) => {
           console.log(`Result for postal code: ${code}`);
-          console.log(res.data.results);
+          const results = res.data.results;
+          console.log(
+            "Sending data to local mysql database: ",
+            results
+          );
+          results.map((company)=> {
+            console.log(company)
+          })
           console.log("---------------------------------");
         });
     }
-    /* capitalRegionPostalCode.map((code) => {
-      axios
-        .get(
-          `http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=2&resultsFrom=0&streetAddressPostCode=${code}&companyRegistrationFrom=2014-02-28`
-        )
-        .then((res) => {
-          console.log(`Result for postal code: ${code}`);
-          console.log(res.data.results);
-          console.log("---------------------------------");
-        });
-    }); */
   };
-  useEffect(() => {
-    axios.get("http://localhost:4000").then((res) => {
-      console.log("backend connected");
-    });
-  }, []);
+
+  const testPostData = () => {
+    const testData = {
+      name: "Knowway oy",
+      businessId: "3353271-6",
+      registrationDate: "2023-03-21",
+      companyForm: "OY",
+      detailsUri: "http://avoindata.prh.fi/opendata/bis/v1/3353271-6",
+    };
+    axios
+      .post("http://localhost:4000/addCompany", testData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getAll = async () => {
+    await axios
+      .get("http://localhost:4000/getAll")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const syncDatabase = () => {
+    axios
+      .get("http://localhost:4000/")
+      .then((res) => {
+        console.log("Sync data done");
+      })
+      .catch((error) => console.log(error));
+  }
+
+  
 
   return (
     <div className="App">
       <h1>API Client</h1>
+      <div>
+        <button onClick={syncDatabase}>Sync Database</button>
+      </div>
       <p>Press the below button to fetch companies data from the API:</p>
-
       <button onClick={fetchAPIdata}>Fetch API data</button>
       <div>
         {businessData.map((business) => {
           return <p>{business.name}</p>;
         })}
       </div>
+      <div>
+        <button onClick={testPostData}>Test post data</button>
+      </div>
+      <div>
+        <button onClick={getAll}>Get data from Mysql DB</button>
+      </div>
       <p>Output: </p>
-{/*       {businessData.map()}
- */}    </div>
+      {/*       {businessData.map()}
+       */}{" "}
+    </div>
   );
 }
 

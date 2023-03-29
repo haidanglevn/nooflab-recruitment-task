5c7e9ff0603671108acc36818184a5193509da0c
Dang Le <haidang.levn@gmail.com>
2023-03-29 11:51:56 +0300

    Add output visualization in table and JSON format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,17 @@ import "./App.css";
 function App() {
   const [businessData, setBusinessData] = useState([]);
   const [getPostalCode, setGetPostalCode] = useState();
+  const [response, setResponse] = useState();
 
-  console.log("Looking for postal code...", getPostalCode);
+  // To sync once everytime frontend is loaded
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/")
+      .then((res) => {
+        console.log("Sync data done");
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   const capitalRegionPostalCode = [
     "02100",
@@ -20,16 +29,16 @@ function App() {
     "00510",
     "00180",
   ];
+
   const fetchAPIdata = async () => {
-    axios
-      .get("http://localhost:4000/")
-      .then((res) => {
-        console.log("Sync data done");
-      })
-      .catch((error) => console.log(error));
     console.log("Fetching API data");
 
-    await axios.get("http://localhost:4000/fetchCompanies");
+    await axios.get("http://localhost:4000/fetchCompanies").then((res) => {
+      console.log("Response from server: ", res);
+      setResponse(
+        "Companies data is ready and stored in database! Step 2 is ready. "
+      );
+    });
   };
 
   const getAll = async () => {
@@ -37,6 +46,7 @@ function App() {
       .get("http://localhost:4000/getAll")
       .then((res) => {
         console.log(res.data);
+        setBusinessData(res.data);
       })
       .catch((error) => console.log(error));
   };
@@ -46,46 +56,98 @@ function App() {
       await axios
         .get(`http://localhost:4000/postal_codes/${getPostalCode}/companies`)
         .then((res) => {
-          setBusinessData(res.data)
+          setBusinessData(res.data);
         })
         .catch((err) => console.log(err));
     } else {
-      console.log('Choose a postal code')
+      console.log("Choose a postal code");
     }
   };
 
   return (
     <div className="App">
-      <h1>API Client</h1>
-      <p>Press the below button to fetch companies data from the API:</p>
-      <button onClick={fetchAPIdata}>Fetch API data</button>
-     
-      <div>
-        <p>Get all data from Mysql DB</p>
-        <button onClick={getAll}>Get All</button>
-        <h2>OR</h2>
-        <label htmlFor="postal-code">
-          Select a postal code from the list and press get data:
-        </label>
-        <select
-          name="postal-code"
-          onChange={(e) => setGetPostalCode(e.target.value)}
-        >
-          <option value="" defaultChecked>
-            Choose a postal code
-          </option>
-          {capitalRegionPostalCode.map((postalCode) => {
-            return <option value={postalCode}>{postalCode}</option>;
-          })}
-        </select>
-        <button onClick={getOne}>Get data from Mysql DB</button>
-      </div>
-      <p>Output: </p>
-      <div>
-        {businessData.map((business)=> {
-          return <p>{business.name}</p> 
-        })}
-      </div>
+      <h1>PHR API Client</h1>
+      <main>
+        <div className="step1">
+          <h2>Step 1: </h2>
+          <p>Press the below button to fetch companies data from the API:</p>
+          <p>
+            Fetching postal codes: 02100, 00140, 00930, 00710, 01730, 00500,
+            01760, 01690, 00510, 00180
+          </p>
+          <button onClick={fetchAPIdata}>Fetch API data</button>
+          <h2>Output: </h2>
+          <div className="output">{response ? <p>{response}</p> : ""}</div>
+        </div>
+
+        <div className="step2">
+          <h2>Step 2: </h2>
+          <p>Get all data from Mysql DB</p>
+          <button onClick={getAll}>Get All</button>
+          <h2>OR</h2>
+          <label htmlFor="postal-code">
+            Select a postal code from the list and press get data:
+          </label>
+          <br />
+          <select
+            name="postal-code"
+            onChange={(e) => setGetPostalCode(e.target.value)}
+          >
+            <option value="" defaultChecked>
+              Choose a postal code
+            </option>
+            {capitalRegionPostalCode.map((postalCode) => {
+              return <option value={postalCode}>{postalCode}</option>;
+            })}
+          </select>{" "}
+          <br />
+          <p></p>
+          <button onClick={getOne}>Get data</button>
+          <h2>GET request: </h2>
+          {getPostalCode == undefined ? (
+            <p>GET /postal_codes/[CODE]/companies</p>
+          ) : (
+            <p>{`/postal_codes/${getPostalCode}/companies`}</p>
+          )}
+          <h2>Output in table: </h2>
+          <div>
+            <table>
+              <thead>
+                <tr key={"header"}>
+                  <th>Business ID</th>
+                  <th>Name</th>
+                  <th>Registration Date</th>
+                  <th>Company Form</th>
+                  <th>Postal Code</th>
+                </tr>
+              </thead>
+              <tbody>
+                {businessData.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.businessId}</td>
+                    <td>{item.name}</td>
+                    <td>{item.registrationDate}</td>
+                    <td>{item.companyForm}</td>
+                    <td>{item.postalCode}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <h2>Output in JSON format: </h2>
+          <div className="output">
+            {businessData.length > 0 ? (
+              <div>
+                {businessData.map((business) => {
+                  return <p>{JSON.stringify(business)}</p>;
+                })}
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
